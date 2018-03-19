a3e3b51d08942d53801dd2a5d1dd2cbe3888234a
Mansour Albaibi <mansouralbaibi@mansours-air.attlocal.net>
2018-03-18 21:01:40 -0500

    1st week
    1st week

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -1,11 +1,18 @@
 /*
   	ISYS 320
-  	Name(s):
+  	Name(Mansour Albaibi):
   	Date: 
 */
 
 // 4. Your pseudocode algorithm for how to break down the figure
-
+/*
+  step1: create a class named StarFigures.
+  step2: create three public method named pattern1, pattern2, and pattern3.
+  step3: In pattern1 method:
+  a. create a two for loop structure that will print two lines of 5 stars each without spacing.
+  b. outer loop control number of lines to be printed.
+  
+*/
 
 public class StarFigures {
 
--- src/Tricky.java
+++ src/Tricky.java
@@ -1,17 +1,43 @@
+import javax.print.DocFlavor.STRING;
+
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(Mansour Albaibi):
+  	Date: 3/16/2018
 */
 
 // 1. Your predicted output 
- 
+/*
+   This is message1.
+   This is message2.
+   This is message1.
+   Done with message2.
+   Done with main. 
+*/
 
-public class Tricky {
+
+public class Tricky{
+  public static void main(STRING[] args) {
+	message1();
+	message2();
+	System.out.println("Done with main.");
+}
+
+public static void message1() {
+	System.out.println("This is message1.");
 
 }
 
-// 3. Were you correct? Explain any differences
- 
- 
+public static void message2() {
+	System.out.println("This is message2.");
+	message1();
+	System.out.println("Done with message2.");
+  }
+  
+}
 
+// 3. Were you correct? Explain any differences
+/* Prediction is entirely correct.
+  Reason:
+  message1() is a public method of same class Tricky which can be access in method message2().
+ */
